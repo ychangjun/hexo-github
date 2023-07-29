37d2783d56244b7c50ccac614163be61b78af318
ycjun <443789721@qq.com>
2023-07-29 08:44:48 +0800

    网站基础成型

--- source/js/title.js
+++ source/js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '咦？人去哪了？';
+    document.title = '🙈咦？人去哪了？';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '哦！原来在这';
+    document.title = '🤣哦！原来你在这';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
